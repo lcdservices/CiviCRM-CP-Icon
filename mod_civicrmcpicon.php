f3c0b24b377d7f251fde77cf817ffd3786e3129c
Brian Shaughnessy <brian@lcdservices.biz>
2011-11-28 22:41:44 -0500

    update for better j17 compatibility

--- mod_civicrmcpicon.php
+++ mod_civicrmcpicon.php
@@ -30,11 +30,23 @@ if (!defined( '_JOS_CIVICRMCPICON_MODULE' ))
     }
 
     ?>
-    <div id="cpanel">
+    <div class="icon-wrapper" id="civicrmicon">
         <?php
 			$link = 'index.php?option=com_civicrm&task=civicrm/dashboard&reset=1';
 			quickiconButtonCiviCRM( $link, 'icon-48-civicrm.png', JText::_( 'CiviCRM Home' ) );
         ?>
     </div>
+    <!--Borrowed this method of injecting into the quick icons panel from Akeeba Backup (www.akeebabackup.com)-->
+    <script lang="text/javascript">
+	var civicrmIcon = $('civicrmicon');
+	try {
+		var civicrmIconParent = $('civicrmicon').getParent().getParent();
+		if(civicrmIconParent.attributes.class.textContent == 'panel') {
+			civicrmIconParent.setStyle('display','none');
+		}
+		$('cpanel').grab(civicrmIcon);
+	} catch(e) {	
+	}
+    </script>
     <?php
 }
